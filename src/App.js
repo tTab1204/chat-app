af9c17adf4c05a20438200d9306091c8435d6836
JuYoung <ken44929@gmail.com>
2021-07-01 01:21:14 +0900

    Initial development environment setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ChatPage from '@pages/ChatPage/ChatPage';
+import LoginPage from '@pages/LoginPage/LoginPage';
+import SignupPage from '@pages/SignupPage/SignupPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={ChatPage} />
+        <Route path='/login' component={LoginPage} />
+        <Route path='/sign-up' component={SignupPage} />
+      </Switch>
+    </Router>
   );
 }
 
