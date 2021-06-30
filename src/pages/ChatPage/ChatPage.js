7dec76c78adbd416c5b2ccb70b88e98c1abb5a0f
JuYoung <ken44929@gmail.com>
2021-07-01 01:20:09 +0900

    create ChatPage

---
+++ src/pages/ChatPage/ChatPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'antd';
+const ChatPage = () => {
+  return (
+    <>
+      <Button type='primary'>무야호</Button>
+    </>
+  );
+};
+
+export default ChatPage;
