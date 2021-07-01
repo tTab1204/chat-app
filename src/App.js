f67d1fe1abd065ddeeea136b798303a7fea73a21
JuYoung <ken44929@gmail.com>
2021-07-02 00:45:05 +0900

    redux store에 유저 정보 저장

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
+import firebase from '@/firebase';
+import { useDispatch, useSelector } from 'react-redux';
 import ChatPage from '@pages/ChatPage/ChatPage';
 import LoginPage from '@pages/LoginPage/LoginPage';
 import SignupPage from '@pages/SignupPage/SignupPage';
+import { setUser } from '@redux/actions/user_actions';
+import Loading from '@components/Loading/Loading';
 
 function App() {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state.user?.isLoading);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        history.push('/');
+        dispatch(setUser(user));
+      } else history.push('/login');
+    });
+  }, []);
+
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={ChatPage} />
-        <Route path='/login' component={LoginPage} />
-        <Route path='/sign-up' component={SignupPage} />
-      </Switch>
-    </Router>
+    <>
+      {isLoading && <Loading />}
+      {!isLoading && (
+        <Switch>
+          <Route exact path='/' component={ChatPage} />
+          <Route path='/login' component={LoginPage} />
+          <Route path='/sign-up' component={SignupPage} />
+        </Switch>
+      )}
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
 import Reducer from '@redux/reducers';
@@ -20,7 +21,9 @@ ReactDOM.render(
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
       )}
     >
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
