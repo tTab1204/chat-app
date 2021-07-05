58034a1144c2eee369c44c2ba0f2046808f4a13a
JuYoung <ken44929@gmail.com>
2021-07-05 16:00:31 +0900

    firebase DB에 저장된 메시지들 불러오기

--- src/components/MainPanel/MainPanel.js
+++ src/components/MainPanel/MainPanel.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import MessageHeader from '@components/MessageHeader/MessageHeader';
+import React, { useEffect, useState } from 'react';
 import Message from '@components/Message/Message';
 import MessageForm from '@components/MessageForm/MessageForm';
 import { MainPanelContainer, MessageBox } from './MainPanelStyle';
+import { useSelector } from 'react-redux';
+import firebase from '@/firebase';
 
 const MainPanel = () => {
+  const chatRoom = useSelector((state) => state.chatRoom.currentChatRoom);
+  const user = useSelector((state) => state.user.currentUser);
+  const messagesRef = firebase.database().ref('messages');
+
+  const [messages, setMessages] = useState([]);
+  const [messagesLoading, setMessagesLoading] = useState(true);
+
+  const addMessagesListeners = async (chatRoomId) => {
+    let messagesArray = [];
+    await messagesRef.child(chatRoomId).on('child_added', (DataSnapshot) => {
+      messagesArray.push(DataSnapshot.val());
+      setMessages(messagesArray);
+      setMessagesLoading(false);
+    });
+  };
+
+  useEffect(() => {
+    if (chatRoom) addMessagesListeners(chatRoom.id);
+  }, [chatRoom, messagesLoading]);
+
   return (
     <MainPanelContainer>
-      <MessageBox></MessageBox>
+      <MessageBox>
+        {messages?.map((message, i) => (
+          <Message key={i} message={message} user={user} />
+        ))}
+      </MessageBox>
       <MessageForm />
     </MainPanelContainer>
   );
