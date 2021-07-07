c7ce44e38f0ada37b75261fa82b2f5b0e33c1599
JuYoung <ken44929@gmail.com>
2021-07-07 23:02:00 +0900

    채팅 검색 기능 추가

--- src/components/MainPanel/MainPanel.js
+++ src/components/MainPanel/MainPanel.js
@@ -2,20 +2,26 @@ import React, { useEffect, useState } from 'react';
 import Message from '@components/Message/Message';
 import MessageForm from '@components/MessageForm/MessageForm';
 import { MainPanelContainer, MessageBox } from './MainPanelStyle';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { showMessages } from '@redux/actions/messages_actions';
+import { Switch, Route } from 'react-router-dom';
 import firebase from '@/firebase';
+import DirectMessages from '@components/DirectMessages/DirectMessages';
 
 const MainPanel = () => {
+  const dispatch = useDispatch();
   const chatRoom = useSelector((state) => state.chatRoom.currentChatRoom);
   const user = useSelector((state) => state.user.currentUser);
+  const filteredMessages = useSelector((state) => state.messages.filteredMessages);
   const messagesRef = firebase.database().ref('messages');
 
   const [messages, setMessages] = useState([]);
-  const [messagesLoading, setMessagesLoading] = useState(true);
+  const [, setMessagesLoading] = useState(true);
 
   const addMessagesListeners = async (chatRoomId) => {
     let messagesArray = [];
     await messagesRef.child(chatRoomId).on('child_added', (DataSnapshot) => {
+      // console.log(DataSnapshot.val());
       messagesArray.push(DataSnapshot.val());
       setMessages(messagesArray);
       setMessagesLoading(false);
@@ -23,14 +29,22 @@ const MainPanel = () => {
   };
 
   useEffect(() => {
-    if (chatRoom) addMessagesListeners(chatRoom.id);
-  }, [chatRoom, messagesLoading]);
+    if (chatRoom) {
+      addMessagesListeners(chatRoom.id);
+      dispatch(showMessages(messages, chatRoom.name));
+    }
+
+    // return () => console.log('component unmounting');
+  }, [messages.length]);
 
   return (
     <MainPanelContainer>
       <MessageBox>
-        {messages?.map((message, i) => (
-          <Message key={i} message={message} user={user} />
+        {!filteredMessages &&
+          messages?.map((message) => <Message key={message.id} message={message} user={user} />)}
+
+        {filteredMessages?.map((filteredMessage, i) => (
+          <Message key={i} message={filteredMessage} user={user} />
         ))}
       </MessageBox>
       <MessageForm />
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,16 +1,57 @@
-import React from 'react';
-import { SearchContainer, LogoLink, Searchbox } from './SearchbarStyle';
+import React, { useEffect, useState } from 'react';
+import {
+  SearchContainer,
+  LogoLink,
+  Searchbox,
+  ChannelName,
+  MenuBox,
+  SearchWrapper,
+} from './SearchbarStyle';
 import chatAppLogo from '@assets/chat-app-logo.png';
 import searchIcon from '@assets/search.png';
+import { useDispatch, useSelector } from 'react-redux';
+import { MenuOutlined } from '@ant-design/icons';
+import { showFilteredMessages } from '@redux/actions/messages_actions';
+import { isVisibleMenu } from '@redux/actions/menu_actions';
 
 const Searchbar = () => {
+  const dispatch = useDispatch();
+  const currentChannel = useSelector((state) => state.chatRoom.currentChatRoom);
+  const messages = useSelector((state) => state.messages.messages);
+
+  const [, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState(messages);
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+
+    const matchValue = messages.filter((message) => message.content?.includes(e.target.value));
+    setSearchResults(matchValue);
+  };
+
+  const handleMenuClick = () => {
+    setShowMenu(!showMenu);
+
+    dispatch(isVisibleMenu(showMenu));
+  };
+  useEffect(() => {
+    dispatch(showFilteredMessages(searchResults));
+  }, [searchResults, dispatch]);
+
   return (
     <SearchContainer>
-      <LogoLink to='/'>
-        <img src={chatAppLogo} alt='chat-app-logo' />
-      </LogoLink>
-      <img src={searchIcon} alt='search-icon' />
-      <Searchbox type='text' placeholder='Search...' />
+      <SearchWrapper>
+        <MenuBox>
+          <MenuOutlined onClick={handleMenuClick} />
+        </MenuBox>
+        <LogoLink to='/'>
+          <img src={chatAppLogo} alt='chat-app-logo' />
+        </LogoLink>
+        <ChannelName># {currentChannel?.name}</ChannelName>
+        <img src={searchIcon} alt='search-icon' />
+        <Searchbox type='text' onChange={handleSearchChange} placeholder='Search...' />
+      </SearchWrapper>
     </SearchContainer>
   );
 };
